4b40c7b8021e0706663b4f02f8c0730bf4cc46cc
Gearoid Murphy <gearoidmurphy2000@gmail.com>
2021-12-30 22:18:38 +0000

    pushing up prototype 1 version

--- Src/code.py
+++ Src/code.py
@@ -1,29 +1,28 @@
-##########################################
-# MLX90640 Thermal Camera w Raspberry Pi
-# -- 2fps with Interpolation and Blitting
-##########################################
-#
 import time,board,busio
 import numpy as np
 import adafruit_mlx90640
 import matplotlib.pyplot as plt
+from numpy.lib.function_base import average
 from scipy import ndimage
+from datetime import datetime
 
-file = open('/home/pi/Desktop/data.txt', 'w')
+
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
 mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_32_HZ#REFRESH_16_HZ # set refresh rate
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ # set refresh rate
 mlx_shape = (24,32) # mlx90640 shape
 
 mlx_interp_val = 10 # interpolate # on each dimension
 mlx_interp_shape = (mlx_shape[0]*mlx_interp_val,
                     mlx_shape[1]*mlx_interp_val) # new shape
 
-fig = plt.figure(figsize=(12,9)) # start figure
+file = open('data.txt', 'a')
+
+fig = plt.figure(figsize=(12,7)) # start figure
 ax = fig.add_subplot(111) # add subplot
 fig.subplots_adjust(0.05,0.05,0.95,0.95) # get rid of unnecessary padding
-therm1 = ax.imshow(np.zeros(mlx_interp_shape),interpolation='nearest',
-                   cmap=plt.cm.jet,vmin=25,vmax=45) # preemptive image
+therm1 = ax.imshow(np.zeros(mlx_interp_shape),interpolation='none',
+                   cmap=plt.cm.jet,vmin=0,vmax=60) # preemptive image
 cbar = fig.colorbar(therm1) # setup colorbar
 cbar.set_label('Temperature [$^{\circ}$C]',fontsize=14) # colorbar label
 
@@ -39,20 +38,33 @@ def plot_update():
     data_array = ndimage.zoom(data_array,mlx_interp_val) # interpolate
     therm1.set_array(data_array) # set data
     therm1.set_clim(vmin=np.min(data_array),vmax=np.max(data_array)) # set bounds
-    cbar.on_mappable_changed(therm1) # update colorbar range
+    cbar.update_normal(therm1) # update colorbar range
 
-    file.write(therm1)
-
+    
     ax.draw_artist(therm1) # draw new thermal image
     fig.canvas.blit(ax.bbox) # draw background
     fig.canvas.flush_events() # show the new image
+    #fig.show()
     return
+
+def calcAverage():
+    hightemps = 0
+    counter = 0
+    for h in range(24):
+        for w in range(32):
+            t = frame[h * 32 + w]
+            if t > 21:
+                hightemps = hightemps+t
+                counter = counter + 1
+    aveageOfHightemps = hightemps/counter
+    return aveageOfHightemps
 
 t_array = []
 while True:
     t1 = time.monotonic() # for determining frame rate
     try:
         plot_update() # update plot
+        averageTemp = calcAverage()
     except:
         continue
     # approximating frame rate
@@ -60,3 +72,8 @@ while True:
     if len(t_array)>10:
         t_array = t_array[1:] # recent times for frame rate approx
     print('Frame Rate: {0:2.1f}fps'.format(len(t_array)/np.sum(t_array)))
+    print('Average foot Temperature: '+ str(averageTemp))
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    file.write(dt_string+'\t'+'Average Temperature: ' + str(averageTemp)+'\n')
+    file.flush()
